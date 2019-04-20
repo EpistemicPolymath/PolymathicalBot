1514354cf1d65de097ffe4ca99352621c669b37a
EpistemicPolymath <epistemicpolymath@gmail.com>
2019-04-19 20:27:02 -0400

    Added commands command, but will edit how it works soon.

--- polybot.js
+++ polybot.js
@@ -70,4 +70,21 @@ client.on('chat', (channel, user, message, self) => {
       client.action('epistemicpolymath', 'https://www.speedrun.com/jak1#100');
     }
 
+    if(message === "!commands") {
+        client.action('epistemicpolymath',
+      `
+        !hello - say hello to the bot!
+
+        !github - gives you the Github link for the bot!
+
+        !jak1boards - gives you the link to Jak 1 boards on speedrun.com
+
+        !hundoboards - gives you the link to Jak 100% boards on speedrun.com
+
+        !commands - lists all available commands
+
+        More commands to come :D
+      `);
+    }
+
 });
