ab627b2b9fd4554d6b9a4337112c47d240f39b9c
EpistemicPolymath <epistemicpolymath@gmail.com>
2019-05-15 15:25:34 -0400

    Updated and fixed the hello command

--- commands/helloCommand.js
+++ commands/helloCommand.js
@@ -1,18 +1,26 @@
 // Say hello to the user that uses the !hello command
 const helloCommand = (message, user) => {
-  if (message === '!hello' && user['mod'] === false && !user['badges-raw'].includes("broadcaster")) {
-      return `Hey, ${user[`display-name`]}!`;
-    }
+  if (message !== "!hello") {
+    return;
+  }
 
-    // Say hello and thank a user that is a mod that uses !hello command
-    if (message === '!hello' && user['mod'] === true && !user['badges-raw'].includes("broadcaster")) {
-      return `Hey, ${user[`display-name`]}! Thank you for being an awesome mod :D`;
-    }
+  // If just a regular user without any badges
+  if (!user['badges-raw']) {
+    return `Hey, ${user[`display-name`]}!`;
+  }
 
-    // Message to braodcaster
-    if (message === '!hello' && user['badges-raw'].includes("broadcaster")) {
-      return `Hey ${user[`display-name`]}! Thank you for being an awesome broadcaster!`
-    }
+  // If the user is a mod
+  if (user['mod'] === true) {
+    return `Hey, ${user[`display-name`]}! Thank you for being an awesome mod :D`;
+  }
+
+  // If the user is the broadcaster
+  if (
+    user['badges-raw'] &&
+    user['badges-raw'].includes("broadcaster")
+  ) {
+    return `Hey ${user[`display-name`]}! Thank you for being an awesome broadcaster!`;
+  }
 }
 
 module.exports = helloCommand;
