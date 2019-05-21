db0787ae17bd236f2f2e26c75975190aadbbfb16
EpistemicPolymath <epistemicpolymath@gmail.com>
2019-05-21 15:07:43 -0400

    Updated helloCommand.js

--- commands/helloCommand.js
+++ commands/helloCommand.js
@@ -14,13 +14,24 @@ const helloCommand = (message, user) => {
     return `Hey, ${user[`display-name`]}! Thank you for being an awesome mod :D`;
   }
 
-  // If the user is the broadcaster
-  if (
-    user['badges-raw'] &&
-    user['badges-raw'].includes("broadcaster")
-  ) {
-    return `Hey ${user[`display-name`]}! Thank you for being an awesome broadcaster!`;
-  }
+  // If the user has badges
+  if (user['badges-raw']) {
+
+      // If user has badges, but they are not a broadcaster / subscriber
+      if (!user['badges-raw'].includes("broadcaster") ||
+          !user['badges-raw'].includes("subscriber")
+         ) {
+          return `Hey ${user[`display-name`]}! Nice badge(s)!`;
+      }
+
+      // If the user is the broadcaster
+      if (user['badges-raw'].includes("broadcaster")) {
+          return `Hey ${user[`display-name`]}! Thank you for being an awesome broadcaster!`;
+        // If the user is a subscriber
+      } else if (user['badges-raw'].includes("subscriber")) {
+          return `Hey ${user[`display-name`]}! Thank you for being a sub!`;
+      }
+    }
 }
 
 module.exports = helloCommand;
