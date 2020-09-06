c3b96636cbf63732f8696195549ff3b5ffdf9fd4
EpistemicPolymath <epistemicpolymath@gmail.com>
2020-09-06 12:41:39 -0400

    Edited the sub goal interval message

--- intervals/subGoalMessage.js
+++ intervals/subGoalMessage.js
@@ -1,6 +1,6 @@
 // Tells the Viewers to say !hello to the lonely bot
 const subGoalMessage = () => {
-    return `I am working on a new emote set of my favorite/adored video game characters. Check out what is next with !subgoal`;
+    return `I have a set of emotes based of my favorite/adored video game characters. Check out what is next with !subgoal`;
 };
 
 module.exports = subGoalMessage;
