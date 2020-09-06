12f1f931370cdde046dffbe2960c4d665a537584
EpistemicPolymath <epistemicpolymath@gmail.com>
2020-09-06 12:43:45 -0400

    Fixed a spelling error in the sub goal interval message

--- intervals/subGoalMessage.js
+++ intervals/subGoalMessage.js
@@ -1,6 +1,6 @@
 // Tells the Viewers to say !hello to the lonely bot
 const subGoalMessage = () => {
-    return `I have a set of emotes based of my favorite/adored video game characters. Check out what is next with !subgoal`;
+    return `I have a set of emotes based off my favorite/adored video game characters. Check out what is next with !subgoal`;
 };
 
 module.exports = subGoalMessage;
