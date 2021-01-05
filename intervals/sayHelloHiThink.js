f7a35d876b6722d5a3cefad7dfe854bbb1e9ad0b
EpistemicPolymath <epistemicpolymath@gmail.com>
2021-01-05 13:52:53 -0500

    Updated the episteHI emote to episteWave so had to change some text in commands

--- intervals/sayHelloHiThink.js
+++ intervals/sayHelloHiThink.js
@@ -1,6 +1,6 @@
 // Tells the Viewers to say !hello to the lonely bot
 const sayHelloHiThink = () => {
-    return "You can say Hello to polymathicalbot with: !hello. You can also spam emotes with !hi + !think + !hype, but be careful it costs 100 polyz :D";
+    return "You can say Hello to polymathicalbot with: !hello. You can also spam emotes with !wave + !think + !hype, but be careful it costs 100 polyz :D";
 };
 
 module.exports = sayHelloHiThink;
