1d1ed3a2e0378515365830e471753b7ea4ac993a
EpistemicPolymath <epistemicpolymath@gmail.com>
2021-01-05 14:02:56 -0500

    Updated the duscord interval message

--- intervals/discordCommunity.js
+++ intervals/discordCommunity.js
@@ -1,6 +1,6 @@
 // Discord Interval Link for the Bot
 const discordCommunity = () => {
-    return `Need help learning Jak? Want to chat and get to know each other off-stream? Join the discord community: https://discord.gg/fR7U3ZP`;
+    return `Want to discuss learning Jak? Want to chat and get to know each other off-stream? Join the discord community: https://discord.gg/fR7U3ZP`;
 }
 
 module.exports = discordCommunity;
