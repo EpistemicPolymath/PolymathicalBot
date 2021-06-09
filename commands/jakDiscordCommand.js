981d785acd9c87ecb4a8627e5c0ad32def0e0eb5
EpistemicPolymath <epistemicpolymath@gmail.com>
2021-06-09 11:50:20 -0400

    Updated Jak Speeedrunning Discord invite

--- commands/jakDiscordCommand.js
+++ commands/jakDiscordCommand.js
@@ -1,7 +1,7 @@
 // Gives invite link for the Jak Speedruns Discord
 const jakDiscordCommand = (message) => {
     if (message === '!jakdiscord') {
-      return 'https://discord.gg/HUzXuNn';
+      return 'https://discord.gg/v8QNJFZ9';
   }
 }
 
